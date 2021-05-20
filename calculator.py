3c50f530045ef079f68179a91e11ebaaf6ffe622
joe shajan <joeshajan1551@gmail.com>
2021-05-20 19:31:12 +0530

    4th button added

---
+++ calculator.py
@@ -0,0 +1,23 @@
+from tkinter import *
+window = Tk()
+window.geometry("500x500")
+
+
+def changetext():
+    a.config(text='button clicked')
+
+
+button1 = Button(window, text="ok", width=5, height=1, bg="yellow", command=changetext)
+button2 = Button(window, text="ok", width=5, height=1, bg="yellow", command=changetext)
+button3 = Button(window, text="ok", width=5, height=1, bg="yellow", command=changetext)
+button4 = Button(window, text="ok", width=5, height=1, bg="yellow", command=changetext)
+
+a = Label(window, text="welcome")
+
+button1.grid(row=0, column=0)
+button2.grid(row=0, column=1)
+button3.grid(row=0, column=2)
+button4.grid(row=0, column=3)
+
+
+window.mainloop()
