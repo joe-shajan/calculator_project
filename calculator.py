05e48f21943592bc075e301a041d106d4c3cb69b
joeshajan <joeshajan1551@gmail.com>
2021-05-22 15:45:21 +0530

    third row added

--- calculator.py
+++ calculator.py
@@ -32,35 +32,41 @@ input_field.grid(row=0,column=0)
 input_field.pack(ipady=10)
 
 #frame for buttons
-btns_frame = Frame(win, width=312,height=272.5,bg="grey")
+btns_frame = Frame(win, width=312,height=272.5,bg="#fff")
 btns_frame.pack()
 
 #first row
-clear = Button(btns_frame, text = "C", fg = "black", width = 24, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: bt_clear()).grid(row = 0, column = 0, columnspan = 3, padx = 1, pady = 1)
-divide = Button(btns_frame, text="/", fg="black", width=8, height=3, bd=0, bg="#eee", cursor="hand2",
-                command=lambda: btn_click("/")).grid(row=0, column=3, padx=1, pady=1)
+clear = Button(btns_frame, text = "C", fg = "black", width = 24, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: bt_clear()).grid(row = 0, column = 0, columnspan = 3)
+divide = Button(btns_frame, text="/", fg="black", width=6, height=3, bd=0, bg="#fff", cursor="hand2",
+                command=lambda: btn_click("/")).grid(row=0, column=3)
 
 #second row
-seven = Button(btns_frame, text="7", fg="black", width=8, height=3, bd=0, bg="#fff", cursor="hand2",
-               command=lambda: btn_click(7)).grid(row=1, column=0, padx=1, pady=1)
+seven = Button(btns_frame, text="7", fg="black", width=6, height=3, bd=0, bg="#fff", cursor="hand2",
+               command=lambda: btn_click(7)).grid(row=1, column=0)
 
-eight = Button(btns_frame, text="8", fg="black", width=8, height=3, bd=0, bg="#fff", cursor="hand2",
-               command=lambda: btn_click(8)).grid(row=1, column=1, padx=1, pady=1)
+eight = Button(btns_frame, text="8", fg="black", width=6, height=3, bd=0, bg="#fff", cursor="hand2",
+               command=lambda: btn_click(8)).grid(row=1, column=1)
 
-nine = Button(btns_frame, text="9", fg="black", width=8, height=3, bd=0, bg="#fff", cursor="hand2",
-              command=lambda: btn_click(9)).grid(row=1, column=2, padx=1, pady=1)
+nine = Button(btns_frame, text="9", fg="black", width=6, height=3, bd=0, bg="#fff", cursor="hand2",
+              command=lambda: btn_click(9)).grid(row=1, column=2)
 
-multiply = Button(btns_frame, text="*", fg="black", width=8, height=3, bd=0, bg="#eee", cursor="hand2",
-                  command=lambda: btn_click("*")).grid(row=1, column=3, padx=1, pady=1)
+multiply = Button(btns_frame, text="*", fg="black", width=6, height=3, bd=0, bg="#fff", cursor="hand2",
+                  command=lambda: btn_click("*")).grid(row=1, column=3)
 
-button1 = Button(win, text="ok", width=5, height=1, bg="yellow", )
-button2 = Button(win, text="ok", width=5, height=1, bg="yellow",)
-button3 = Button(win, text="ok", width=5, height=1, bg="yellow", )
-button4 = Button(win, text="ok", width=5, height=1, bg="yellow", )
-button5 = Button(win, text="ok", width=5, height=1, bg="yellow", )
-button6 = Button(win, text="ok", width=5, height=1, bg="yellow", )
 
-a = Label(win, text="welcome")
+#thirdrow
+four = Button(btns_frame, text="4", fg="black", width=6, height=3, bd=0, bg="#fff", cursor="hand2",
+               command=lambda: btn_click(4)).grid(row=1, column=0)
+
+five = Button(btns_frame, text="5", fg="black", width=6, height=3, bd=0, bg="#fff", cursor="hand2",
+               command=lambda: btn_click(5)).grid(row=1, column=1)
+
+six = Button(btns_frame, text="6", fg="black", width=6, height=3, bd=0, bg="#fff", cursor="hand2",
+              command=lambda: btn_click(6)).grid(row=1, column=2)
+
+subtract = Button(btns_frame, text="*", fg="black", width=6, height=3, bd=0, bg="#fff", cursor="hand2",
+                  command=lambda: btn_click("-")).grid(row=1, column=3)
+
 
 
 
